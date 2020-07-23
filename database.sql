7ba1eb44332c28f8ed87053b755e8a6062fa2d74
Yasuhiro Suzuki <yasszu@yahoo.com>
2020-07-23 16:49:56 +0900

    create models

--- database.sql
+++ database.sql
@@ -1,33 +1,11 @@
-CREATE TABLE IF NOT EXISTS `todo`.`list`
-(
-    `id`    BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS `todo`.`item`
-(
-    `id`      BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `title`   VARCHAR(255) NOT NULL,
-    `checked` TINYINT(4)   NOT NULL DEFAULT 0,
-    `list_id` BIGINT(20)   NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `list_id_idx` (`list_id` ASC) VISIBLE,
-    CONSTRAINT `list_id`
-        FOREIGN KEY (`list_id`)
-            REFERENCES `todo`.`list` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
-
-INSERT INTO todo.list (title)
+INSERT INTO lists (title)
 VALUES ('List1'),
        ('List2');
 
-INSERT INTO todo.item (title, list_id)
+INSERT INTO items (title, list_id)
 VALUES ('item1', 1),
-       ('item2', 2),
-       ('item3', 2);
+       ('item2', 1),
+       ('item3', 2),
+       ('item4', 2),
+       ('item5', 2);
 
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,21 @@
 use serde::Serialize;
+use diesel::Queryable;
 
 #[derive(Serialize)]
 pub struct Status {
     pub status: String
-}   
+}
+
+#[derive(Queryable)]
+pub struct List {
+    pub id: i32,
+    pub title: String,
+}
+
+#[derive(Queryable)]
+pub struct Item {
+    pub id: i32,
+    pub title: String,
+    pub checked: bool,
+    pub list_id: i32,
+}
