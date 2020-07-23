fd4b5133b985f9a95bfc3fa7004c4c5e23d6596b
Yasuhiro Suzuki <yasszu@yahoo.com>
2020-07-23 13:56:18 +0900

    update migration

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,22 @@
 table! {
+    items (id) {
+        id -> Bigint,
+        title -> Varchar,
+        checked -> Tinyint,
+        list_id -> Bigint,
+    }
+}
+
+table! {
     lists (id) {
         id -> Bigint,
         title -> Varchar,
     }
 }
+
+joinable!(items -> lists (list_id));
+
+allow_tables_to_appear_in_same_query!(
+    items,
+    lists,
+);
